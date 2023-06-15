a61b853adad90422a96debd44f719b67abae5873
Minister02 <109047810+Minister02@users.noreply.github.com>
2023-06-15 21:18:11 +0200

    Add files via upload

---
+++ aplikacja2/demo/src/main/java/com/example/demo/data/Travel.java
@@ -0,0 +1,16 @@
+package com.example.demo.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
+public class Travel {
+    private int id;
+    private String name;
+    private String departureDate;
+    private String returnDate;
+    private String travelTasks;
+}
